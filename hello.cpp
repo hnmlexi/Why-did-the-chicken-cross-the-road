b1b2fd885dba78dd1c31620bfa5127de1d2d92df
Lexi <nonsugarfan@proton.me>
2025-12-12 13:26:44 +0100

    hi i made this. its my first time using c++.

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Why did the chicken cross the road?" << std::endl;
+
+    std::cin >> std::ws; 
+    std::string input;
+    std::getline(std::cin, input); 
+    
+    if (input == "to get to the other side"){
+        std::cout << "fk u, you already know it!" << std::endl;
+    } else {
+        std::cout << "To get to the other side!" << std::endl;
+    }
+    
+    std::cout << "Press Enter to exit..." << std::endl; 
+    std::cin.get();
+    return 0;
+}
